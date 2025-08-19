aa8917d89382a5040652442772b46e11584d3f18
rtylerdavis <tdavis197@gmail.com>
2025-08-18 20:17:31 -0500

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import WordleSolver from "./WordleSolver";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <WordleSolver />
+  </React.StrictMode>
+);
